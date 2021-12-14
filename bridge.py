0cc59766a7c3899c32f0948e7ae33c825ca244d4
ShubhamRaj1997 <shubhamrajputcool@gmail.com>
2021-12-15 01:06:43 +0530

    added bridge pattern

---
+++ bridge.py
@@ -0,0 +1,57 @@
+"""
+It separates the abstraction from implementations and they can actually grow and change independently
+Eg.
+like in music player
+There is on home you have view of Artist, Genre, Trending ->  and their different views
+resources <=> views can happen, cartesian products we have
+
+Basically it separates the business logic (maybe anything) from the actual implementation, the similar thing is done by
+strategy design pattern for dependency inversion but here we can have common implementations for different but similar
+resources
+"""
+from abc import ABC, abstractmethod
+
+
+class Thumbnail(ABC):
+    @abstractmethod
+    def get_thumbnail(self):
+        pass
+
+
+class SmallThumbnail(Thumbnail):
+    def get_thumbnail(self):
+        pass
+
+
+class LargeThumbnail(Thumbnail):
+    def get_thumbnail(self):
+        pass
+
+
+class Media(ABC):
+    @abstractmethod
+    def get_preview(self):
+        pass
+
+
+class Video(Media):
+    def __init__(self, thumbnail: Thumbnail):
+        self.thumbnail = thumbnail
+
+    def get_preview(self):
+        self.thumbnail.get_thumbnail()
+
+
+class Image(Media):
+    def __init__(self, thumbnail: Thumbnail):
+        self.thumbnail = thumbnail
+
+    def get_preview(self):
+        self.thumbnail.get_thumbnail()
+
+
+a = Image(SmallThumbnail()).thumbnail
+b = Video(SmallThumbnail()).thumbnail
+
+c = Image(LargeThumbnail()).thumbnail
+d = Video(LargeThumbnail()).thumbnail
