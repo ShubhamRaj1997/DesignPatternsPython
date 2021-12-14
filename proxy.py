46e7b5058628247994331deb4faf4a793910a4e6
ShubhamRaj1997 <shubhamrajputcool@gmail.com>
2021-12-15 00:41:56 +0530

    proxy design pattern

---
+++ proxy.py
@@ -0,0 +1,33 @@
+"""
+Proxy pattern is useful for
+* lazy loading for class
+* caching
+* logging
+
+"""
+from abc import ABC, abstractmethod
+
+
+class Subject(ABC):
+
+    @abstractmethod
+    def do_something(self):
+        pass
+
+
+class OriginalSubject(Subject):
+
+    def do_something(self):
+        pass
+
+
+class ProxySubject(Subject):
+    def __init__(self, subject: Subject):
+        self._subject = subject
+
+    def do_something(self):
+        if self.can_access():
+            self._subject.do_something()
+
+    def can_access(self):
+        return True
